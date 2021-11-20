4bb681dee86abb8cc40690bcc79f28e237307b6c
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2021-11-20 23:08:29 +0100

    Add foreign key constrains on users in SQL template.

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,3 +1,16 @@
+CREATE TABLE roles (
+  role_id SERIAL UNIQUE
+  , role TEXT
+);
+
+CREATE TABLE users (
+  user_id SERIAL UNIQUE
+  , email TEXT UNIQUE
+  , phone TEXT UNIQUE
+  , role_id INTEGER REFERENCES roles (role_id)
+  , creation_time TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
 CREATE TABLE temperature (
   temperature_id serial
   , hostname text
@@ -6,32 +19,20 @@ CREATE TABLE temperature (
   , capture_time timestamp with time zone
   , temperature double precision NOT NULL
   , server_time timestamp with time zone DEFAULT now()
-  , owner_id integer -- ADD: contraint to users.user_id
+  , owner_id integer REFERENCES users (user_id)
   , uuid uuid UNIQUE -- UNIQUE allows multiply NULL values
 );
 
 CREATE INDEX capture_time_idx ON temperature (capture_time);
 CREATE INDEX temperature_id_idx ON temperature (temperature_id);
 
-CREATE TABLE roles (
-  role_id SERIAL
-  , role TEXT
-);
-
 INSERT INTO roles (role) VALUES
   ('admin'),
   ('user');
 
-CREATE TABLE users (
-  user_id SERIAL
-  , email TEXT UNIQUE
-  , phone TEXT UNIQUE
-  , role_id INTEGER
-  , creation_time TIMESTAMP WITH TIME ZONE DEFAULT now()
-);
 
 CREATE TABLE access_keys (
   key_id SERIAL
-  , owner_id INTEGER
+  , owner_id INTEGER REFERENCES users (user_id)
   , key TEXT UNIQUE
 );
