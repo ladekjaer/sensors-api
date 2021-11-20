603d0c21194f42447d69f6b88cf7bc0be001553e
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2021-11-20 23:19:59 +0100

    Cleanup in SQL template.

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,10 +1,10 @@
 CREATE TABLE roles (
-  role_id SERIAL UNIQUE
+  role_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
   , role TEXT
 );
 
 CREATE TABLE users (
-  user_id SERIAL UNIQUE
+  user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
   , email TEXT UNIQUE
   , phone TEXT UNIQUE
   , role_id INTEGER REFERENCES roles (role_id)
@@ -12,15 +12,15 @@ CREATE TABLE users (
 );
 
 CREATE TABLE temperature (
-  temperature_id serial
-  , hostname text
-  , thermometer_id text -- NORMALIZE
-  , pi_id text -- NORMALIZE
-  , capture_time timestamp with time zone
-  , temperature double precision NOT NULL
-  , server_time timestamp with time zone DEFAULT now()
-  , owner_id integer REFERENCES users (user_id)
-  , uuid uuid UNIQUE -- UNIQUE allows multiply NULL values
+  temperature_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+  , hostname TEXT
+  , thermometer_id TEXT -- NORMALIZE
+  , pi_id TEXT -- NORMALIZE
+  , capture_time TIMESTAMP WITH TIME ZONE
+  , temperature DOUBLE PRECISION NOT NULL
+  , server_time TIMESTAMP WITH TIME ZONE DEFAULT now()
+  , owner_id INTEGER REFERENCES users (user_id)
+  , uuid UUID UNIQUE -- UNIQUE allows multiply NULL values
 );
 
 CREATE INDEX capture_time_idx ON temperature (capture_time);
@@ -32,7 +32,7 @@ INSERT INTO roles (role) VALUES
 
 
 CREATE TABLE access_keys (
-  key_id SERIAL
+  key_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY
   , owner_id INTEGER REFERENCES users (user_id)
   , key TEXT UNIQUE
 );
